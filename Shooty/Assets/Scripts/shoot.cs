37e02fa260ff2b1a5818d2c51a6d8aaa2e23ceef
Christian Smith <fashionbatman@gmail.com>
2013-09-01 01:37:10 -0700

    Small Changes
    Separated the player movement and the gun into separated scripts to make
    it easier to test new guns.

---
+++ Shooty/Assets/Scripts/shoot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour 
+{
+	public GameObject bullet;
+	public float bulletSpeed = 1.0f;
+	public float shootDelay = 0.2f;
+	
+	public bool canShoot = true;
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//shootyshoot
+		if((Input.GetAxis("FireHorizontal") != 0.0f || Input.GetAxis("FireVertical") != 0.0f) && canShoot)
+		{
+			Vector3 shootDirection = new Vector3(Input.GetAxis("FireHorizontal"),Input.GetAxis("FireVertical"), 0).normalized;
+			GameObject bulletInstance = Instantiate(bullet,transform.position,Quaternion.LookRotation(shootDirection)) as GameObject;
+			bulletInstance.rigidbody.AddForce(shootDirection*bulletSpeed,ForceMode.VelocityChange);
+			canShoot = false;
+			Invoke("ShootDelay", shootDelay);
+		}
+			
+	}
+	
+	void ShootDelay()
+	{
+		canShoot = true;	
+	}
+}
