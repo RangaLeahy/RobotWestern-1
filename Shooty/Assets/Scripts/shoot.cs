05daa32e80035a0628a6a777d8f3c32ee2346cd2
AstronautG117 <Greg@Newton.local>
2013-09-01 21:51:12 -0700

    Updated Shoot
    Multiple modes added
    Ready for mouse input using Vector 3

--- Shooty/Assets/Scripts/shoot.cs
+++ Shooty/Assets/Scripts/shoot.cs
@@ -1,32 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class shoot : MonoBehaviour 
-{
+public class Shoot : MonoBehaviour {
+	
+	public Vector3 mouseDirection = new Vector3(0,0,1); //Temporary direction in place of mouse input.
+	
 	public GameObject bullet;
 	public float bulletSpeed = 1.0f;
-	public float shootDelay = 0.2f;
+	public float mode1Frequency = 5.0f; //In bullets per second.
 	
+	public int shootMode = 1;
 	public bool canShoot = true;
 	
-	
 	// Update is called once per frame
-	void Update () 
-	{
-		//shootyshoot
-		if((Input.GetAxis("FireHorizontal") != 0.0f || Input.GetAxis("FireVertical") != 0.0f) && canShoot)
-		{
-			Vector3 shootDirection = new Vector3(Input.GetAxis("FireHorizontal"), 0,Input.GetAxis("FireVertical")).normalized;
-			GameObject bulletInstance = Instantiate(bullet,transform.position,Quaternion.LookRotation(shootDirection)) as GameObject;
-			bulletInstance.rigidbody.AddForce(shootDirection*bulletSpeed,ForceMode.VelocityChange);
-			canShoot = false;
-			Invoke("ShootDelay", shootDelay);
+	void Update () {
+		
+		if (Input.GetKeyDown("1")) {
+			shootMode = 1;
+		} else if (Input.GetKeyDown("2")) {
+			shootMode = 2;
+		} //Sets the shoot mode.
+		
+		if (shootMode == 1) {
+			if (Input.GetKey("mouse 0") && canShoot) {
+				shoot(mouseDirection);
+				canShoot = false;
+				Invoke("shootDelay",(1/mode1Frequency));
+			}
+		} else if (shootMode == 2) {
+			if (Input.GetKeyDown ("mouse 0") && canShoot) {
+				shoot(mouseDirection);
+			}
 		}
-			
 	}
 	
-	void ShootDelay()
-	{
-		canShoot = true;	
+	void shoot(Vector3 shootDirection) {//Shoots a bullet in the shootDirection
+		GameObject bulletInstance = Instantiate(bullet,transform.position,Quaternion.LookRotation(shootDirection)) as GameObject;
+		bulletInstance.rigidbody.AddForce(shootDirection*bulletSpeed,ForceMode.VelocityChange);
 	}
+		
+	void shootDelay() {
+			canShoot = true;
+	}
+			
 }
